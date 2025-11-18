f094eec529bf5b65f14826205d0d84734cfc3aa3
Kaden5480 <kaden5480@protonmail.com>
2025-11-18 20:41:11 +0000

    Allow setting canvases to be on top

--- src/components/internal/Canvas.cs
+++ src/components/internal/Canvas.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using UnityEngine;
 
 using UECanvas = UnityEngine.Canvas;
@@ -6,13 +8,15 @@ using UEGraphicRaycaster = UnityEngine.UI.GraphicRaycaster;
 
 namespace UILib {
     internal class Canvas : BaseComponent {
+        private const int minSortingOrder = 1000;
+        private static List<UECanvas> canvases = new List<UECanvas>();
+
         private UECanvas canvas;
         private UECanvasScaler scaler;
 
         internal Canvas() {
             canvas = root.AddComponent<UECanvas>();
             canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-            canvas.sortingOrder = 100;
 
             scaler = root.AddComponent<UECanvasScaler>();
             scaler.uiScaleMode = UECanvasScaler.ScaleMode.ScaleWithScreenSize;
@@ -20,6 +24,53 @@ namespace UILib {
             scaler.screenMatchMode = UECanvasScaler.ScreenMatchMode.Expand;
 
             root.AddComponent<UEGraphicRaycaster>();
+
+            Register(canvas);
+        }
+
+        /**
+         * <summary>
+         * Set this canvas to be on top.
+         * </summary>
+         */
+        internal void BringToFront(Canvas canvas) {
+            SetOnTop(canvas.canvas);
+        }
+
+        /**
+         * <summary>
+         * Sets a canvas to be above all others.
+         * </summary>
+         */
+        private static void SetOnTop(UECanvas canvas) {
+            // Try finding the canvas
+            int index = canvases.IndexOf(canvas);
+
+            if (index < 0) {
+                return;
+            }
+
+            // Iterate the list in reverse, decrementing all sorting orders
+            // until reaching the canvas to set on top
+            for (int i = canvases.Count - 1; i > index; i--) {
+                canvases[i].sortingOrder--;
+            }
+
+            // Now remove the canvas from the list, and add it back
+            // to the end, while also updating the sorting order
+            canvases.Remove(canvas);
+            canvas.sortingOrder = minSortingOrder + canvases.Count;
+            canvases.Add(canvas);
+        }
+
+        /**
+         * <summary>
+         * Registers a canvas so it can be sorted.
+         * </summary>
+         */
+        private static void Register(UECanvas canvas) {
+            canvas.sortingOrder = minSortingOrder + canvases.Count;
+            canvases.Add(canvas);
         }
     }
 }

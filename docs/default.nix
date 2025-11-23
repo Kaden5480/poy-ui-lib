62f9b0eb7400d58969ff650747186856492ee7a5
Kaden5480 <kaden5480@protonmail.com>
2025-11-23 23:31:03 +0000

    Add support for docs

---
+++ docs/default.nix
@@ -0,0 +1,18 @@
+let
+    pkgs = import <nixpkgs> {};
+in
+    pkgs.mkShell {
+        buildInputs = with pkgs; [
+            dotnet-sdk
+            dotnet-runtime
+        ];
+
+        shellHook = ''
+        if [ ! -d .config ]; then
+            dotnet new tool-manifest
+            dotnet tool update --local docfx
+        fi
+
+        dotnet docfx
+        '';
+    }

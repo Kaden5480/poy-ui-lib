b6f8278f60a2fc054f4494e48a365e9d3e5d76ff
Kaden5480 <kaden5480@protonmail.com>
2025-11-23 20:32:18 +0000

    Update colors

--- src/Colors.cs
+++ src/Colors.cs
@@ -3,33 +3,32 @@ using UnityEngine.UI;
 
 namespace UILib {
     public static class Colors {
-        public static Color white       = new Color(1f, 1f, 1f);
-        public static Color black       = new Color(0f, 0f, 0f);
-        public static Color grey        = new Color(12f/255f, 20f/255f, 20f/255f);
-        public static Color lightGrey   = new Color(134f/255f, 134f/255f, 134f/255f);
-        public static Color lighterGrey = new Color(164f/255f, 164f/255f, 164f/255f);
+        public static Color white        = new Color(1f, 1f, 1f);
+        public static Color black        = new Color(0f, 0f, 0f);
+        public static Color darkGrey     = new Color(22f/255f, 22f/255f, 22f/255f);
+        public static Color grey         = new Color(50f/255f, 50f/255f, 50f/255f);
+        public static Color lightGrey    = new Color(80f/255f, 80f/255f, 80f/255f);
+        public static Color lighterGrey  = new Color(134f/255f, 134f/255f, 134f/255f);
+        public static Color lightestGrey = new Color(164f/255f, 164f/255f, 164f/255f);
 
-        public static Color red              = new Color(204f/255f, 51f/255f, 51f/255f);
-        public static Color lightRed         = new Color(166f/255f, 89f/255f, 89f/255f);
+        public static Color red          = new Color(204f/255f, 51f/255f, 51f/255f);
+        public static Color lightRed     = new Color(166f/255f, 89f/255f, 89f/255f);
 
-        public static ColorBlock colorBlock = new ColorBlock() {
-                fadeDuration     = 0.1f,
-                colorMultiplier  = 1f,
-                normalColor      = Colors.lightGrey,
-                selectedColor    = Colors.lightGrey,
-                disabledColor    = Colors.lightGrey,
-                highlightedColor = Colors.lighterGrey,
-                pressedColor     = Colors.lighterGrey,
-        };
+        public static ColorBlock colorBlock      = MakeBlock(grey, lightGrey);
+        public static ColorBlock lightColorBlock = MakeBlock(lightGrey, lighterGrey);
+        public static ColorBlock darkColorBlock  = MakeBlock(darkGrey, grey);
+        public static ColorBlock redColorBlock   = MakeBlock(lightRed, red);
 
-        public static ColorBlock redColorBlock = new ColorBlock() {
-                fadeDuration     = 0.1f,
-                colorMultiplier  = 1f,
-                normalColor      = Colors.lightRed,
-                selectedColor    = Colors.lightRed,
-                disabledColor    = Colors.lightRed,
-                highlightedColor = Colors.red,
-                pressedColor     = Colors.red,
-        };
+        private static ColorBlock MakeBlock(Color normal, Color selected) {
+            return new ColorBlock() {
+                    fadeDuration     = 0.1f,
+                    colorMultiplier  = 1f,
+                    normalColor      = normal,
+                    selectedColor    = normal,
+                    disabledColor    = normal,
+                    highlightedColor = selected,
+                    pressedColor     = selected,
+            };
+        }
     }
 }

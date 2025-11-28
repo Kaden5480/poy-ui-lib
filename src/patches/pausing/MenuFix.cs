cd260e04914aa2b04dc5b122cb8860138066d41c
Kaden5480 <kaden5480@protonmail.com>
2025-11-28 00:56:53 +0000

    Prevent InGameMenu toggling when waiting for input

--- src/patches/pausing/MenuFix.cs
+++ src/patches/pausing/MenuFix.cs
@@ -6,10 +6,24 @@ namespace UILib.Patches {
      * Makes sure the <see cref="PauseHandler"/> runs after
      * `InGameMenu`, to make sure the game actually
      * pauses correctly.
+     *
+     * Also prevent execution while <see cref="InputOverlay"/>
+     * is reading an input.
      * </summary>
      */
     [HarmonyPatch(typeof(InGameMenu), "Update")]
     internal static class MenuFix {
+        // Don't run when the input overlay is
+        // waiting for an input
+        private static bool Prefix() {
+            if (InputOverlay.waitingForInput == true) {
+                return false;
+            }
+
+            return true;
+        }
+
+        // Update the PauseHandler
         private static void Postfix() {
             PauseHandler.Update();
         }

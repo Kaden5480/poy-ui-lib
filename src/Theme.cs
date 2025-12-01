c7d5ab52d567326e52003b9a6e88d424c7085c49
Kaden5480 <kaden5480@protonmail.com>
2025-12-01 20:08:30 +0000

    Add HSL support

--- src/Theme.cs
+++ src/Theme.cs
@@ -88,16 +88,66 @@ namespace UILib {
 
         /**
          * <summary>
-         * Helper for more easily making a `Color`
-         * from RGBA values.
+         * Function for helping with hsl conversions.
          *
-         * `r`, `g`,`b` must be between 0-255 inclusive.
-         * `a` must be between 0-100 inclusive.
+         * See:
+         * https://en.wikipedia.org/wiki/HSL_and_HSV#Color_conversion_formulae
          * </summary>
-         * <param name="r">The value of the red component</param>
-         * <param name="g">The value of the green component</param>
-         * <param name="b">The value of the blue component</param>
-         * <param name="a">The value of the alpha component</param>
+         */
+        private static float HSLF(float h, float s, float l, int n) {
+            float a = s * Math.Min(l, 1 - l);
+            float k = (n + h/30) % 12;
+
+            return l - a * Math.Max(-1,
+                Math.Min(Math.Min(k-3, 9-k), 1)
+            );
+        }
+
+        /**
+         * <summary>
+         * Creates a `Color` from hue, saturation, lightness, and alpha.
+         * </summary>
+         * <param name="h">The hue (0-360)</param>
+         * <param name="s">The saturation (0-100)</param>
+         * <param name="l">The lightness (0-100)</param>
+         * <param name="a">The alpha (0-100)</param>
+         * <returns>The `Color`</returns>
+         */
+        public static Color HSLA(float h, float s, float l, float a) {
+            s /= 100f;
+            l /= 100f;
+            a /= 100f;
+
+            float r = HSLF(h, s, l, 0);
+            float g = HSLF(h, s, l, 8);
+            float b = HSLF(h, s, l, 4);
+
+            return new Color(r, g, b, a);
+        }
+
+        /**
+         * <summary>
+         * Creates a `Color` from hue, saturation, and lightness.
+         * </summary>
+         * <param name="h">The hue (0-360)</param>
+         * <param name="s">The saturation (0-100)</param>
+         * <param name="l">The lightness (0-100)</param>
+         * <returns>The `Color`</returns>
+         */
+        public static Color HSL(float h, float s, float l) {
+            return HSLA(h, s, l, 100f);
+        }
+
+        /**
+         * <summary>
+         * Creates a `Color` from red, green, blue, and alpha
+         * components.
+         * </summary>
+         * <param name="r">The red value (0-255)</param>
+         * <param name="g">The green value (0-255)</param>
+         * <param name="b">The blue value (0-255)</param>
+         * <param name="a">The alpha value (0-100)</param>
+         * <returns>The `Color`</returns>
          */
         public static Color RGBA(int r, int g, int b, int a) {
             return new Color(
@@ -110,14 +160,13 @@ namespace UILib {
 
         /**
          * <summary>
-         * Helper for more easily making a `Color`
-         * from RGB values.
-         *
-         * `r`, `g`,`b` must be between 0-255 inclusive.
+         * Creates a Color from red, green, blue, and alpha
+         * components.
          * </summary>
-         * <param name="r">The value of the red component</param>
-         * <param name="g">The value of the green component</param>
-         * <param name="b">The value of the blue component</param>
+         * <param name="r">The red value (0-255)</param>
+         * <param name="g">The green value (0-255)</param>
+         * <param name="b">The blue value (0-255)</param>
+         * <returns>The `Color`</returns>
          */
         public static Color RGB(int r, int g, int b) {
             return RGBA(r, g, b, 100);

0d8344f5c9d7360e795d77f07b0c1af523e87bc4
Kaden5480 <kaden5480@protonmail.com>
2025-12-06 01:51:05 +0000

    Restore player's velocity after unpausing

--- src/patches/pausing/MenuFix.cs
+++ src/patches/pausing/MenuFix.cs
@@ -1,4 +1,7 @@
+using System.Reflection;
+
 using HarmonyLib;
+using UnityEngine;
 
 namespace UILib.Patches {
     /**
@@ -15,9 +18,23 @@ namespace UILib.Patches {
      */
     [HarmonyPatch(typeof(InGameMenu), "Update")]
     internal static class MenuFix {
+        private static bool pausedPlayer;
+        private static Vector3 playerVelocity;
+
         // Don't run when the input overlay is
         // waiting for an input
-        private static bool Prefix() {
+        private static bool Prefix(bool ___pausedRB, Rigidbody ___playerRB) {
+            // Track the player's velocity
+            if (___pausedRB == false && ___playerRB != null) {
+                playerVelocity = ___playerRB.velocity;
+            }
+
+            // If the in game menu paused the player's velocity
+            // indicate that
+            if (___pausedRB == true) {
+                pausedPlayer = true;
+            }
+
             if (InputOverlay.waitingForInput == true) {
                 return false;
             }
@@ -26,9 +43,22 @@ namespace UILib.Patches {
         }
 
         // Update the PauseHandler
-        private static void Postfix(InGameMenu __instance) {
+        private static void Postfix(
+            InGameMenu __instance,
+            bool ___pausedRB,
+            Rigidbody ___playerRB
+        ) {
             PauseFixes.InGameMenuCheck(__instance);
             PauseHandler.Update();
+
+            if (___playerRB == null) {
+                return;
+            }
+
+            if (___pausedRB == false && pausedPlayer == true) {
+                ___playerRB.velocity = playerVelocity;
+                pausedPlayer = false;
+            }
         }
     }
 }
